f5efa0cf107b86f32a38c409543a976b538a5b9d
toktokeh <toktokeh@gmail.com>
2018-03-25 16:47:20 -0700

    Update

--- solutions/Strings.java
+++ solutions/Strings.java
@@ -1,18 +1,22 @@
 import java.util.*;
 
 public class Strings {
-
+    
     // ***********************************************************************
     // Difficulty: 0
     // Count the number of vowels in a given string
 
     // public static int getCount(String str) {
-
+    //     int vowelsCount = 0;
+    //     for(char c : str.toCharArray()) {
+    //         vowelsCount += (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ? 1 : 0;
+    //     }
+    //     return vowelsCount;
     // }
 
     // Alternative one-line regex solution:
     // public static int getCount(String str) {
-
+    //     return str.replaceAll("(?i)[^aeiou]", "").length();
     // }
 
     // public static void main(String[] args) {
@@ -30,7 +34,8 @@ public class Strings {
     //     String str = "Java Exercises!";
     //     System.out.println("Original String = " + str);
     //     // Get the character at positions 0 and 10.
-
+    //     int index1 = str.charAt(0);
+    //     int index2 = str.charAt(10);
 
     //     // Print out the results.
     //     System.out.println("The character at position 0 is " + (char) index1);
@@ -42,7 +47,7 @@ public class Strings {
     // Compare two strings lexicographically.
 
     // public static int compare(String str1, String str2) {
-
+    //     return str1.compareTo(str2);
     // }
 
     // public static void main(String[] args) {
@@ -59,7 +64,7 @@ public class Strings {
     // Compare two strings lexicographically, ignoring case differences.
 
     // public static int compare(String str1, String str2) {
-
+    //     return str1.compareToIgnoreCase(str2);
     // }
 
     // public static void main(String[] args) {
@@ -76,7 +81,7 @@ public class Strings {
     // Concatenate a given string to the end of another string. (Do not use '+')
 
     // public static String concatenate(String str1, String str2) {
-
+    //     return str1.concat(str2);
     // }
 
     // public static void main(String[] args) {
@@ -90,7 +95,7 @@ public class Strings {
     // Test if a given string contains the specified sequence of char values.
 
     // public static boolean contains(String str1, String str2) {
-
+    //     return str1.contains(str2);
     // }
 
     // public static void main(String[] args) {
@@ -105,7 +110,7 @@ public class Strings {
     // Create a new String object with the contents of a character array.
 
     // public static String createString(char[] chars) {
-
+    //     return String.valueOf(chars);
     // }
 
     // public static void main(String[] args) {
@@ -118,7 +123,7 @@ public class Strings {
     // Check whether a given string ends with the contents of another string.
     
     // public static boolean ends(String str1, String str2) {
-
+    //     return str1.endsWith(str2);
     // }
 
     // public static void main(String[] args) {
@@ -134,7 +139,7 @@ public class Strings {
     // Compare if two strings are equal, ignoring case considerations.
     
     // public static boolean compare(String str1, String str2) {
-
+    //     return str1.equalsIgnoreCase(str2);
     // }
 
     // public static void main(String[] args) {
@@ -155,7 +160,10 @@ public class Strings {
     */
 
     // public static void main(String[] args) {
-
+    //     Calendar c = Calendar.getInstance();
+    //     System.out.println("Current Date and Time:");     
+    //     System.out.format("%tB %te, %tY%n", c, c, c);
+    //     System.out.format("%tl:%tM %tp%n", c, c, c);
     // }
 
     // ***********************************************************************
@@ -164,7 +172,7 @@ public class Strings {
 
     // public static void main(String[] args) {
     //     String s1 = "Hello";
-    //     System.out.println(...);
+    //     System.out.println(s1.getBytes());
     // }
 
     // ***********************************************************************
@@ -173,7 +181,7 @@ public class Strings {
 
     // public static void main(String[] args) {
     //     String s1 = "Hello";
-  
+    //     char[] c1 = s1.toCharArray();
     //     System.out.println(c1[0]);
     // }
 
@@ -182,7 +190,10 @@ public class Strings {
     // Return the middle character of the word. If the word's length is even,return the middle 2 characters.
 
     // public static String getMiddle(String word) {
-
+    //     int length = word.length();
+    //     return (length % 2 != 0) 
+    //         ? String.valueOf(word.charAt(length / 2)) 
+    //         : word.substring(length / 2 - 1, length / 2 + 1);
     // }
 
     // public static void main(String[] args) {
@@ -199,7 +210,7 @@ public class Strings {
     // Get the last index of a string within a string.
     
     // public static int getLastIndex(String needle, String haystack) {
-
+    //     return haystack.lastIndexOf(needle, haystack.length()-1);
     // }
 
     // public static void main(String[] args) {
@@ -218,12 +229,12 @@ public class Strings {
 
     //     // Determine whether characters 0 through 7 in str1 
     //     // match characters 28 through 35 in str2.
-
+    //     boolean match1 = str1.regionMatches(0, str2, 28, 8);
 
     //     // Determine whether characters 9 through 15 in str1 
     //     // match characters 9 through 15 in str2.
+    //     boolean match2 = str1.regionMatches(9, str2, 9, 8);
 
-    
     //     // Display the results of the regionMatches method calls.
     //     System.out.println("str1[0 - 7] == str2[28 - 35]? " + match1);  // true
     //     System.out.println("str1[9 - 15] == str2[9 - 15]? " + match2);  // false
@@ -236,7 +247,7 @@ public class Strings {
     //     String str = "The quick brown fox jumps over the lazy dog.";
 
     //     // Replace all the 'd' characters with 'f' characters.
-
+    //     String new_str = str.replace('d', 'f');
 
     //     // Display the strings for comparison.
     //     System.out.println("Original string: " + str);
@@ -252,7 +263,7 @@ public class Strings {
     //     String str = "The quick brown fox jumps over the lazy dog.";
 
     //     // Replace all the 'dog' with 'cat'.
-
+    //     String new_str = str.replace("fox", "cat");
 
     //     // Display the strings for comparison.
     //     System.out.println("Original string: " + str);
@@ -271,8 +282,8 @@ public class Strings {
     //     String startStr = "Red";
 
     //     // Do either of the first two Strings start with startStr?
-
-
+    //     boolean starts1 = str1.startsWith(startStr);
+    //     boolean starts2 = str2.startsWith(startStr);
 
     //     // Display the results of the startsWith calls.
     //     System.out.println(str1 + " starts with " + startStr + "? " + starts1);
@@ -287,7 +298,7 @@ public class Strings {
 
     //     // Get a substring of the above string starting from
     //     // index 10 and ending at index 26.
-
+    //     String new_str = str.substring(10, 26);
 
     //     // Display the two strings for comparison.
     //     System.out.println("old = " + str);
@@ -301,7 +312,7 @@ public class Strings {
     //     String str = "Java Exercises.";
 
     //     // Convert the above string to a char array.
-
+    //     char[] arr = str.toCharArray();
 
     //     // Display the contents of the char array.
     //     System.out.println(arr);
@@ -314,7 +325,7 @@ public class Strings {
     //     String str = "The Quick BroWn FoX!";
 
     //     // Convert the above string to all lowercase.
-
+    //     String lowerStr = str.toLowerCase();
 
     //     // Display the two strings for comparison.
     //     System.out.println("Original String: " + str);
@@ -330,8 +341,8 @@ public class Strings {
 
     //     // Trim the whitespace from the front and back of the
     //     // String.
+    //     String new_str = str.trim();
 
-    
     //     // Display the strings for comparison.
     //     System.out.println("Original String: " + str);
     //     System.out.println("New String: " + new_str);
