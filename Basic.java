9fa82f4f2006ab79de247d8f50d609c85384e73d
toktokeh <toktokeh@gmail.com>
2018-03-25 18:06:18 -0700

    Update

---
+++ Basic.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Basic {
+
+    // ***********************************************************************
+    // Difficulty: 0
+    // convert a decimal number to binary number.
+    // Input Data: 5
+    // Expected Output: 101
+
+    // ***********************************************************************
+    // Difficulty: 0
+    // convert a decimal number to hexadecimal number.
+    // Input Data: 5
+    // Expected Output: 101
+
+    // ***********************************************************************
+    // Difficulty: 0
+    // convert a decimal number to octal number.
+    // Input Data: 5
+    // Expected Output: 101
+
+    // ***********************************************************************
+    // Difficulty: 0
+    // convert a binary number to a decimal number.
+    // Input Data: 5
+    // Expected Output: 101
+
+    // ***********************************************************************
+    // Difficulty: 0
+    // Add two binary numbers.
+
+    // public static long getCount(long m, long n) {
+        
+    //     return 0;
+    // }
+
+    // public static void main(String[] args) {
+    //     long m = 100010;
+    //     long n = 110010;
+    //     System.out.println("m + n = " + getCount(m, n));    // 1010100
+    // }
+
+    // ***********************************************************************
+    // Difficulty: 0
+    // Multiply two binary numbers.
+
+    // public static long getCount(long m, long n) {
+
+    //     return 0;
+    // }
+
+    // public static void main(String[] args) {
+    //     long m = 100010;
+    //     long n = 110010;
+    //     System.out.println("m + n = " + getCount(m, n));    // 1010100
+    // }
+}
